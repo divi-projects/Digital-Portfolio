932f0d0d3cad73c2f1791a298d37de11ba57c53b
Divyansh Gupta <divyanshgupta103@gmail.com>
2025-09-16 21:01:57 +0530

    Initial commit : My Digital Portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     console.log('JavaScript is linked and working!');
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+    alert('JavaScript is linked and working!');
+});
